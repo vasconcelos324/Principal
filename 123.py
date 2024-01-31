b2875aee7e04ee596f4d153f6a4fb90799b01953
vasconcelos324 <129629278+vasconcelos324@users.noreply.github.com>
2024-01-30 21:58:43 -0300

    Add files via upload

--- 123.py
+++ 123.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
 # Criação dos dados
 def extracao_bcb(codigo, inicio, fim):
+    # Substituir espaços por %20
+    inicio = inicio.strftime("%Y-%m-%d")
+    fim = fim.strftime("%Y-%m-%d")
+    
     url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json&dataInicial={}&dataFinal={}'.format(codigo, inicio, fim)
     df = pd.read_json(url)
     df.set_index('data', inplace=True)
@@ -22,52 +27,20 @@ def main():
 
     series_temporais = {
         "Indice de Vendas do Varejo": 1455,
-         "Indice de Confiança do Consumidor":4393,
-        "Indice de Confiança do Empresario Industrial":7341,
-        "Indice do Preço ao consumidor":13522,
-        "Indice de Comodite Brasil (IC-BR)":27574,
-        "Indice de Comodite Brasil-Metal":27576,
-        "Indice de Comodite Brasil- Energia":27577,
-        "SELIC":1178,
-        "Vendas de Veiculos - Carro Pequeno":7384,
-        "Vendas de Veiculos - Comercias Leves - Hilux/Toro":7385,
-        "Vendas de Veiculos - Caminhões":7386,
-        "Vendas de Veiculos - Onibus":7387,
-        "Vendas de Veiculos - Maquina Agrìcolas":7388,
-        "Vendas de Veiculos - Motociclos":1381,
-        "Vendas de Veiculos - Total - Pequeno/Comerciais/Motociclos":1377,
-        "Produção de Veiculos - Comercias Leves":1374,
-        "Produção de Veiculos - Caminhões":1375,
-        "Produção de Veiculos - Onibus":1376,
-        "Produção de Veiculos - Maquina Agrìcolas":1376,
-        "Produção de Veiculos - Motociclos":1388,
-        "Produção de Veiculos Automotivo Total":1373,
-        "Produção de Derivado do Petroleo - Óleo Bruto Barris/dia (mil)":1389,
-        "Produção de Derivado do Petroleo - LGN Barris/dia (mil)":1390,
-        "Produção de Derivado do Petroleo - Gás Naturual Barris/dia (mil)":1392,
-        "Produção de Derivado do Petroleo - Total Barris/dia (mil)": 1391,
-        "Produção de Derivado do Petroleo - Gasolina Barris/dia (mil)":1393,
-        "Produção de Derivado do Petroleo - Gás de Cozinha Barris/dia (mil)":1394,
-        "Produção de Derivado do Petroleo - Óleo Combustivel Barris/dia (mil)":1395,
-        "Produção de Derivado do Petroleo - Óleo Diesel Barris/dia (mil)":1396,
-        "Produção de Derivado do Petroleo - Outros Barris/dia (mil)":1397,
-        "Produção de Derivado do Petroleo - Total Barris/dia (mil)":1398,
-        "Consumo de Energia Eletrica - comercial (GWH) ":1402,
-        "Consumo de Energia Eletrica - Residencial (GWH)":1403,
-        "Consumo de Energia Eletrica - Industrial (GWH)":1404,
-        "Consumo de Energia Eletrica - Outros (GWH)":1405,
-        "Consumo de Energia Eletrica - Total (GWH)":1406, 
-        "Taxa de Desemprego- Brasil ":24369,
-        "Endividamentto Familiar":29037
-        
+        # ... (outras séries temporais)
     }
 
     opcao_serie = st.selectbox("Escolha uma série temporal:", list(series_temporais.keys()))
 
     codigo_serie = series_temporais[opcao_serie]
 
-    data_inicial = st.date_input("Selecione a data inicial:")
-    data_final = st.date_input("Selecione a data final:")
+    # Permitindo entrada manual da data inicial
+    data_inicial_input = st.text_input("Digite a data inicial (DD/MM/AAAA):")
+    data_inicial = datetime.strptime(data_inicial_input, "%d/%m/%Y") if data_inicial_input else None
+
+    # Permitindo entrada manual da data final
+    data_final_input = st.text_input("Digite a data final (DD/MM/AAAA):")
+    data_final = datetime.strptime(data_final_input, "%d/%m/%Y") if data_final_input else None
 
     if st.button("Consultar Série Temporal"):
         if data_inicial and data_final:
@@ -75,7 +48,7 @@ def main():
 
             if not df_resultado.empty:
                 # Layout em duas colunas
-                col1, col2 = st.beta_columns(2)
+                col1, col2 = st.columns(2)
 
                 # Coluna 1: Exibindo a planilha
                 with col1:
@@ -91,4 +64,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
