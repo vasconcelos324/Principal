3cf0758e8c772ae199a8adea49acd00ded6a7218
vasconcelos324 <129629278+vasconcelos324@users.noreply.github.com>
2024-01-30 22:12:44 -0300

    Add files via upload

--- 123.py
+++ 123.py
@@ -4,11 +4,10 @@ from datetime import datetime
 
 # Criação dos dados
 def extracao_bcb(codigo, inicio, fim):
-    # Substituir espaços por %20
-    inicio = inicio.strftime("%Y-%m-%d")
-    fim = fim.strftime("%Y-%m-%d")
+    inicio_str = inicio.strftime("%Y-%m-%d")
+    fim_str = fim.strftime("%Y-%m-%d")
     
-    url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json&dataInicial={}&dataFinal={}'.format(codigo, inicio, fim)
+    url = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados?formato=json&dataInicial={inicio_str}&dataFinal={fim_str}'
     df = pd.read_json(url)
     df.set_index('data', inplace=True)
     df.index = pd.to_datetime(df.index, dayfirst=True)
@@ -19,19 +18,13 @@ def gerar_grafico(df, nome_serie):
     st.line_chart(df["valor"], use_container_width=True)
     st.title(f"{nome_serie}")
 
-# Criação da Imagem/Titulo/Lista/Caixa da Lista/
+# Criação da Imagem/Título/Lista/Caixa da Lista/
 def main():
-
+    
     st.title("Consulta de Série Temporal do Banco Central")
 
-    series_temporais = {
-        "Indice de Vendas do Varejo": 1455,
-        # ... (outras séries temporais)
-    }
-
-    opcao_serie = st.selectbox("Escolha uma série temporal:", list(series_temporais.keys()))
-
-    codigo_serie = series_temporais[opcao_serie]
+    # Permitindo que o usuário insira a série temporal
+    codigo_serie = st.text_input("Insira o código da série temporal:")
 
     # Permitindo entrada manual da data inicial
     data_inicial_input = st.text_input("Digite a data inicial (DD/MM/AAAA):")
@@ -42,18 +35,24 @@ def main():
     data_final = datetime.strptime(data_final_input, "%d/%m/%Y") if data_final_input else None
 
     if st.button("Consultar Série Temporal"):
-        if data_inicial and data_final:
+        if codigo_serie and data_inicial and data_final:
             df_resultado = extracao_bcb(codigo_serie, data_inicial, data_final)
 
             if not df_resultado.empty:
                 # Layout em duas colunas
-                
-                
-                    gerar_grafico(df_resultado, opcao_serie)
+                col1, col2 = st.columns(2)
+
+                # Coluna 1: Exibindo a planilha
+                with col1:
+                    st.dataframe(df_resultado)
+
+                # Coluna 2: Exibindo o gráfico
+                with col2:
+                    gerar_grafico(df_resultado, f"Série Temporal {codigo_serie}")
             else:
                 st.warning("Não foram encontrados dados para a série temporal informada.")
         else:
-            st.warning("Preencha todos os campos.")
+            st.warning("Preencha todos os campos corretamente.")
 
 if __name__ == "__main__":
     main()
