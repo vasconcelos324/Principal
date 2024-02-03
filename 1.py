97abc74e84e25f76b68f98c30ed881c26733a014
vasconcelos324 <129629278+vasconcelos324@users.noreply.github.com>
2024-02-03 17:12:59 -0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
+st.set_page_config(layout="wide")
+
+
 @st.cache_data
 def busca_titulos_tesouro_direto():
     url = 'https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv'
@@ -22,10 +26,10 @@ tipo_titulo = st.selectbox('Selecione o Tipo do Título', titulos.index.get_leve
 titulos_filtrados = titulos[titulos.index.get_level_values(0) == tipo_titulo]
 
 # Selecionar 'Data de Vencimento'
-data_vencimento = st.selectbox('Selecione a Data de Vencimento', titulos_filtrados.index.get_level_values(1).unique())
+datas_vencimento = st.multiselect('Selecione a(s) Data(s) de Vencimento', titulos_filtrados.index.get_level_values(1).unique())
 
 # Filtrar DataFrame com base na 'Data de Vencimento'
-titulos_filtrados = titulos_filtrados[titulos_filtrados.index.get_level_values(1) == data_vencimento]
+titulos_filtrados = titulos_filtrados[titulos_filtrados.index.get_level_values(1).isin(datas_vencimento)]
 
 # Selecionar uma das colunas restantes
 coluna_selecionada = st.selectbox('Selecione uma coluna', titulos_filtrados.columns)
@@ -37,6 +41,8 @@ col1, col2 = st.columns(2)
 col1.dataframe(titulos_filtrados[[coluna_selecionada]])
 
 # Exibir gráfico de linha da coluna selecionada na segunda coluna usando Plotly
-fig = px.line(titulos_filtrados.reset_index(), x='Data Base', y=coluna_selecionada)
+fig = px.area(titulos_filtrados.reset_index(), x='Data Base', y=coluna_selecionada, color='Data Vencimento', title=coluna_selecionada)
+fig.update_layout(autosize=False, width=1500, height=400)
 col2.plotly_chart(fig)
+col2.markdown("Fonte : Tesouro Direto")
 
