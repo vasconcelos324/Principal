ba73e03a884597c972488a32b87fb01b3d7e2e34
vasconcelos324 <129629278+vasconcelos324@users.noreply.github.com>
2024-02-05 13:16:56 -0300

    Add files via upload

--- banco.py
+++ banco.py
@@ -13,61 +13,7 @@ def extrair_dados(codigo, data_inicial, data_final):
 def main():
     st.title("Consulta de Série Temporal do Banco Central")
     series_temporais = {
-        "Índice volume de vendas no varejo - Brasil                 (Índice)":1455,
-        "Índice de Confiança do Consumidor                          (Índice)":4393,
-        "Índice de Confiança do Empresário Industrial - Geral       (Índice)":7341,
-        "Índice nacional de preços ao consumidor - INPC             (Var. % mensal)":188,
-        "Índice geral de preços do mercado -  IGPM                  (Var. % mensal)":189,
-        "Índice nacional de preços ao consumidor amplo - IPCA       (Var. % mensal)":433,
-        "Índice nacional de preços ao consumidor amplo - IPCA       (%)":13522,
-        "Índice de Comodities - Brasil                              (Índice)":27574,
-        "Índice de Comodite Brasil - Agropecuária                   (Índice)":27575,
-        "Índice de Comodite Brasil - Metal                          (Índice)":27576,
-        "Índice de Comodite Brasil - Energia                        (Índice)":27577,
         
-        "PIB  Agropecuária Taxa de variação real no ano             (Var. % anual)":7327,
-        "PIB  Indústria    Taxa de variação real no ano             (Var. % anual)":7328,
-        "PIB  Serviços     Taxa de variação real no ano             (Var. % anual)":7329,
-        "Dívida Externa Bruta - Pública                             (US$ milhões)" :21523,
-
-        "Taxa de juros - Selic anualizada base 252                                           (% a.a)":1178,
-        "Taxa de desocupação - PNADC                                                         (%)":24369,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - PF           (%)":15888,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - PF        (%)":15889,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - PF    (%)":15890,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - PF        (%)":15891,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - PF             (%)":15892,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - PJ           (%)":15920,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - PJ        (%)":15921,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - PJ    (%)":15922,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - PJ        (%)":15923,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - PJ             (%)":15924,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - Total        (%)":15952,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - Total     (%)":15953,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - Total (%)":15954,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - Total     (%)":15955,
-        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - Total          (%)":15956,
-        "Endividamento das famílias com o SFN em relação à renda acumulada dos últimos doze meses (%)":29037,
-
-        "Vendas de motociclos                                       (Unidades)":1381,
-        "Vendas de veículos pelas concessionárias - Automóveis      (Unidades)":7384,
-        "Vendas de veículos pelas concessionárias - Comerciais leves(Unidades)":7385,
-        "Vendas de veículos pelas concessionárias - Caminhões       (Unidades)":7386,
-        "Vendas de veículos pelas concessionárias - Ônibus          (Unidades)":7387,
-        "Vendas de veículos pelas concessionárias - Total           (Unidades)":7389,
-
-        "Produção total de autoveículos                             (Unidades)":1373,
-        "Produção de automóveis e comerciais leves                  (Unidades)":1374,
-        "Produção de caminhões                                      (Unidades)":1375,
-        "Produção de ônibus                                         (Unidades)":1376,
-        "Produção de motociclos                                     (Unidades)":1377,
-        "Produção de máquinas agrícolas - total                     (Unidades)":1388,
-        
-        "Produção de derivados de petróleo - Óleo bruto             (Barris/dia/mil)":1389,
-        "Produção de derivados de petróleo - LGN                    (Barris/dia/mil)":1390,
-        "Produção de derivados de petróleo - Total                  (Barris/dia/mil)":1391,
-        "Produção de derivados de petróleo - Gás Naturual           (Barris/dia/milhôes)":1392,
-
         "Consumo de derivados de petróleo - Gasolina                (Barris/dia/milhôes)":1393,
         "Consumo de derivados de petróleo - GLP                     (Barris/dia/milhôes)":1394,
         "Consumo de derivados de petróleo - Óleo combustível        (Barris/dia/milhôes)":1395,
@@ -109,8 +55,69 @@ def main():
         "Consumo de Energia Eletrica   Região Sudeste - Residencial (GWH)":1428,
         "Consumo de Energia Eletrica   Região Sudeste - Industrial  (GWH)":1429,
         "Consumo de Energia Eletrica   Região Sudeste - Outros      (GWH)":1430,
-        "Consumo de Energia Eletrica   Região Sudeste - Total       (GWH)":1431}
+        "Consumo de Energia Eletrica   Região Sudeste - Total       (GWH)":1431,
+
+        "Dívida Externa Bruta - Pública                             (US$ milhões)" :21523,
+
+        "Endividamento das famílias com o SFN em relação à renda acumulada dos últimos doze meses (%)":29037,
+
+        "Índice volume de vendas no varejo - Brasil                 (Índice)":1455,
+        "Índice de Confiança do Consumidor                          (Índice)":4393,
+        "Índice de Confiança do Empresário Industrial - Geral       (Índice)":7341,
+        "Índice nacional de preços ao consumidor - INPC             (Var. % mensal)":188,
+        "Índice geral de preços do mercado -  IGPM                  (Var. % mensal)":189,
+        "Índice nacional de preços ao consumidor amplo - IPCA       (Var. % mensal)":433,
+        "Índice nacional de preços ao consumidor amplo - IPCA       (%)":13522,
+        "Índice de Comodities - Brasil                              (Índice)":27574,
+        "Índice de Comodite Brasil - Agropecuária                   (Índice)":27575,
+        "Índice de Comodite Brasil - Metal                          (Índice)":27576,
+        "Índice de Comodite Brasil - Energia                        (Índice)":27577,
+        
+        "PIB  Agropecuária Taxa de variação real no ano             (Var. % anual)":7327,
+        "PIB  Indústria    Taxa de variação real no ano             (Var. % anual)":7328,
+        "PIB  Serviços     Taxa de variação real no ano             (Var. % anual)":7329,
+
+        "Produção total de autoveículos                             (Unidades)":1373,
+        "Produção de automóveis e comerciais leves                  (Unidades)":1374,
+        "Produção de caminhões                                      (Unidades)":1375,
+        "Produção de ônibus                                         (Unidades)":1376,
+        "Produção de motociclos                                     (Unidades)":1377,
+        "Produção de máquinas agrícolas - total                     (Unidades)":1388,
+        
+        "Produção de derivados de petróleo - Óleo bruto             (Barris/dia/mil)":1389,
+        "Produção de derivados de petróleo - LGN                    (Barris/dia/mil)":1390,
+        "Produção de derivados de petróleo - Total                  (Barris/dia/mil)":1391,
+        "Produção de derivados de petróleo - Gás Naturual           (Barris/dia/milhôes)":1392,
+
+        "Reservas internacionais total - Anual                      (US$/milhões)":3545,
+        "Reservas internacionais total - Mensal                     (US$/milhões)":3546,
+
+        "Taxa de juros - Selic anualizada base 252                                           (% a.a)":1178,
+        "Taxa de desocupação - PNADC                                                         (%)":24369,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - PF           (%)":15888,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - PF        (%)":15889,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - PF    (%)":15890,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - PF        (%)":15891,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - PF             (%)":15892,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - PJ           (%)":15920,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - PJ        (%)":15921,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - PJ    (%)":15922,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - PJ        (%)":15923,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - PJ             (%)":15924,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Norte - Total        (%)":15952,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Nordeste - Total     (%)":15953,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Centro-Oeste - Total (%)":15954,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sudeste  - Total     (%)":15955,
+        "Taxa de inadimplência das operações de crédito do SFN - Região Sul - Total          (%)":15956,
+
+        "Vendas de motociclos                                       (Unidades)":1381,
+        "Vendas de veículos pelas concessionárias - Automóveis      (Unidades)":7384,
+        "Vendas de veículos pelas concessionárias - Comerciais leves(Unidades)":7385,
+        "Vendas de veículos pelas concessionárias - Caminhões       (Unidades)":7386,
+        "Vendas de veículos pelas concessionárias - Ônibus          (Unidades)":7387,
+        "Vendas de veículos pelas concessionárias - Total           (Unidades)":7389}
     
+
     opcao_serie = st.selectbox("Escolha uma série temporal:", list(series_temporais.keys()))
     codigo_serie = series_temporais[opcao_serie]
     data_inicial = st.date_input("Digite a data inicial:")
